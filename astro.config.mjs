9d6fde09e2ca0fcf5093bd3a84ecf50de9ddea9c
skordaczuk <slawek.kordaczuk@grandparade.co.uk>
2025-07-10 18:15:31 +0200

    Refactor Astro configuration and update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,62 +12,13 @@ export default defineConfig({
   integrations: [react(), sitemap()],
   server: { port: 3000 },
   vite: {
-    plugins: [
-      tailwindcss(),
-      {
-        name: "message-channel-polyfill",
-        generateBundle(options, bundle) {
-          // Add polyfill to the beginning of worker bundle
-          const polyfill = `
-if (typeof MessageChannel === 'undefined') {
-  globalThis.MessageChannel = class MessageChannel {
-    constructor() {
-      this.port1 = {
-        postMessage: (message) => {
-          setTimeout(() => {
-            if (this.port2.onmessage) {
-              this.port2.onmessage({ data: message });
-            }
-          }, 0);
-        },
-        onmessage: null,
-        start: () => {},
-        close: () => {}
-      };
-      this.port2 = {
-        postMessage: (message) => {
-          setTimeout(() => {
-            if (this.port1.onmessage) {
-              this.port1.onmessage({ data: message });
-            }
-          }, 0);
-        },
-        onmessage: null,
-        start: () => {},
-        close: () => {}
-      };
-    }
-  };
-}
-`;
-
-          for (const fileName in bundle) {
-            const file = bundle[fileName];
-            if (file.type === "chunk" && file.isEntry) {
-              file.code = polyfill + file.code;
-            }
-          }
-        },
-      },
-    ],
+    plugins: [tailwindcss()],
     define: {
       global: "globalThis",
     },
-    resolve: {
-      alias: {
-        // Polyfill for MessageChannel
-        "node:async_hooks": "data:text/javascript,export default {}",
-      },
+
+    ssr: {
+      noExternal: ["react", "react-dom"],
     },
   },
   adapter: cloudflare({
@@ -75,7 +26,4 @@ if (typeof MessageChannel === 'undefined') {
       enabled: true,
     },
   }),
-  experimental: {
-    session: true,
-  },
 });
